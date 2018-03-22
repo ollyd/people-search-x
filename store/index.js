350b87ef4c8bc275e313b89a86223b5b2115c975
Olly Dutton <ollydutton@gmail.com>
2018-03-22 21:59:19 +1100

    added vuex

---
+++ store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as firebase from 'firebase'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    
+  },
+  mutations: {
+    
+  },
+  actions: {
+   
+  },
+  getters: {
+   
+  }
+})
