82ead839288529041ef54d1b7ce87ad8e05e504b
Olly Dutton <ollydutton@gmail.com>
2018-03-26 10:52:22 +1100

    add firebase config disable linting temporarily

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,34 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+	"extends": "eslint-config-airbnb",
+	"parser": "babel-eslint",
+	"env": {
+		"browser": true,
+		"node": true,
+		"mocha": true
+	},
+	"rules": {
+		"indent": [2, "tab"],
+		"no-tabs": 0,
+		"no-console": 0,
+		"no-unused-vars": [
+			"error", { "args": "none" }
+		],
+		"import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+		"comma-dangle": ["error", "never"],
+		"arrow-body-style": 0,
+		"import/no-dynamic-require": "off",
+		"import/prefer-default-export": "off",
+		"no-plusplus": "off",
+		"global-require": "off",
+		"arrow-parens": "off",
+		"no-underscore-dangle": "off",
+		"keyword-spacing": "off",
+		"no-param-reassign": "off",
+		"key-spacing": [
+			"error", { "mode": "minimum" }
+		],
+		"max-len": ["error", 120, { "ignoreStrings": true, "ignoreComments": true, "ignoreTemplateLiterals": true }],
+		"func-names": "off",
+		"quote-props": ["error", "consistent"]
+	}
 }
--- src/main.js
+++ src/main.js
@@ -4,13 +4,22 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import router from './router'
 import { store } from './store'
+import firebaseConfig from '../firebase.config'
 
-Vue.use(Vuetify)
-Vue.config.productionTip = false
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
-})
+  render: h => h(App),
+  created () {
+    firebase.initializeApp(firebaseConfig);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user);
+      }
+    });
+  }
+});
