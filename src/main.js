864c214ea8f71ec935831ce73e44328545f30241
Olly Dutton <ollydutton@gmail.com>
2018-03-25 22:42:39 +1100

    babel config changes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
+import router from './router'
+import { store } from './store'
 
 Vue.use(Vuetify)
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
