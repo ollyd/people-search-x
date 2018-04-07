790f9214e3b8213f518aa37afa7ffebcfa1ce298
Olly Dutton <ollydutton@gmail.com>
2018-04-07 23:09:49 +1000

    wired in algolia index

---
+++ src/components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+	<v-flex xs12 md6 lg3>
+		<v-card tile>
+			<div class="avatar-container">
+				<v-avatar class="cyan darken-3" size="70">
+					<v-icon dark x-large>account_circle</v-icon>
+				</v-avatar>
+			</div>
+			<v-card-title class="grey darken-2" primary-title>
+				<div>
+					<h3 class="title pb-1">Kangaroo Valley Safari</h3>
+					<div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+				</div>
+			</v-card-title>
+			<v-card-actions>
+				<v-btn flat color="cyan accent-1">View Profile</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-flex>
+</template>
+
+<script>
+	export default {
+		// data() {
+		// 	return {
+		// 	}
+		// },
+		// methods: {
+
+		// }
+	}
+</script>
+
+<style scoped>
+	.btn { text-transform: initial; }
+	.avatar-container {
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		height: 54px;
+		border-bottom: 3px solid #84FFFF;
+	}
+	.avatar {
+		position: absolute;
+		top: 20%;
+		left: 50%;
+		transform: translate(-50%,0%);
+		border: 3px solid #84FFFF !important;
+	}
+	.card__title--primary {
+		padding-top: 35px !important;
+		text-align: center;
+	}
+	.card__actions { border-top: 1px solid #84FFFF !important; justify-content: center; }
+	.card__actions .btn { border: 1px solid #84FFFF !important; }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,26 @@
 <template>
 	<v-container grid-list-xl fill-height>
-		<ais-index appId="latency" apiKey="6be0576ff61c053d5f9a3225e2a90f76">
+		<ais-index :index-name="credentials().algoliaIndexName" :appId="credentials().algoliaAppID" :apiKey="credentials().algoliaApiKey">
 			<v-layout row wrap>
 				<v-flex xs12 md4 fill-height>
 					<v-card class="filter-container">
 						<v-layout column>
-							<v-flex>
-								<h3 class="title">
-									Search Filters
-								</h3>
+							<v-flex class="filter-title">
+								<v-icon size="18px">tune</v-icon>
+								<h3 class="subheading">Search Filters</h3>
 							</v-flex>
-							<v-flex>
-								<ais-search-box>
+							<v-flex class="search-box-container">
+								<ais-search-box :searchOnEnterKeyPressOnly="true">
 									<div class="input-group input-group--text-field primary--text" v-bind:class="{ 'input-group--focused': focused }">
 										<label for="name-search">Search by name</label>
 										<div class="input-group__input">
-											<!-- <input class="form-control" id="name-search" type="search"> -->
 											<ais-input
+												attribute-name="Name"
 												v-on:focus.native="onFocus"
 												v-on:blur.native="onBlur"
 												id="name-search"
 												:classNames="{
-												'ais-input': 'form-control'
+													'ais-input': 'form-control'
 												}"
 											/>
 										</div>
@@ -49,7 +48,7 @@
 									<h3 slot="header">Browse by</h3>
 								</ais-tree-menu>
 
-								<ais-price-range attribute-name="price" :classNames="{
+								<ais-price-range attribute-name="Hourly Rate" :classNames="{
 									'ais-price-range__input': 'form-control'
 								}">
 									<h3 slot="header">Price</h3>
@@ -69,7 +68,7 @@
 									<h3 slot="header">Color</h3>
 								</ais-refinement-list>
 							</div>
-							<div class="col-md-10 col-sm-9">
+							<!-- <div class="col-md-10 col-sm-9">
 								<div class="search-controls form-inline">
 									<ais-sort-by-selector :indices="[
 										{name: 'ikea', label: 'Relevance'},
@@ -85,7 +84,7 @@
 
 									<ais-stats/>
 								</div>
-							</div>
+							</div> -->
 						</div>
 					</v-card>
 				</v-flex>
@@ -94,6 +93,7 @@
 						<ais-results>
 							<template slot-scope="{ result }">
 								<div class="search-result">
+									<card></card>
 									<img class="result__image img-responsive" :src="result.image">
 
 									<div class="result__info">
@@ -122,78 +122,6 @@
 							'ais-pagination__item--active': 'active',
 							'ais-pagination__item--disabled': 'disabled'
 						}" v-on:page-change="onPageChange"/> -->
-						<!-- <v-flex xs12 md6 lg3>
-							<v-card tile>
-								<div class="avatar-container">
-									<v-avatar class="cyan darken-3" size="70">
-										<v-icon dark x-large>account_circle</v-icon>
-									</v-avatar>
-								</div>
-								<v-card-title class="grey darken-2" primary-title>
-									<div>
-										<h3 class="title pb-1">Kangaroo Valley Safari</h3>
-										<div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-									</div>
-								</v-card-title>
-								<v-card-actions>
-									<v-btn flat color="cyan accent-1">View Profile</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-flex>
-						<v-flex xs12 md6 lg3>
-							<v-card tile>
-								<div class="avatar-container">
-									<v-avatar class="cyan darken-3" size="70">
-										<v-icon dark x-large>account_circle</v-icon>
-									</v-avatar>
-								</div>
-								<v-card-title class="grey darken-2" primary-title>
-									<div>
-										<h3 class="title pb-1">Kangaroo Valley Safari</h3>
-										<div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-									</div>
-								</v-card-title>
-								<v-card-actions>
-									<v-btn flat color="cyan accent-1">View Profile</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-flex>
-						<v-flex xs12 md6 lg3>
-							<v-card tile>
-								<div class="avatar-container">
-									<v-avatar class="cyan darken-3" size="70">
-										<v-icon dark x-large>account_circle</v-icon>
-									</v-avatar>
-								</div>
-								<v-card-title class="grey darken-2" primary-title>
-									<div>
-										<h3 class="title pb-1">Kangaroo Valley Safari</h3>
-										<div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-									</div>
-								</v-card-title>
-								<v-card-actions>
-									<v-btn flat color="cyan accent-1">View Profile</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-flex>
-						<v-flex xs12 md6 lg3>
-							<v-card tile>
-								<div class="avatar-container">
-									<v-avatar class="cyan darken-3" size="70">
-										<v-icon dark x-large>account_circle</v-icon>
-									</v-avatar>
-								</div>
-								<v-card-title class="grey darken-2" primary-title>
-									<div>
-										<h3 class="title pb-1">Kangaroo Valley Safari</h3>
-										<div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-									</div>
-								</v-card-title>
-								<v-card-actions>
-									<v-btn flat color="cyan accent-1">View Profile</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-flex> -->
 					</v-layout>
 				</v-flex>
 				<!-- <v-layout v-if="loading">
@@ -212,7 +140,13 @@
 </template>
 
 <script>
+	import Card from '~/components/Card';
+	import AlgoliaCreds from '../../algolia.config.js';
+
 	export default {
+		components: {
+			Card
+		},
 		data() {
 			return {
 				focused: false
@@ -224,6 +158,9 @@
 			},
 			onBlur() {
 				this.focused = false;
+			},
+			credentials() {
+				return AlgoliaCreds;
 			}
 		}
 	}
@@ -233,35 +170,12 @@
 	.ais-index { margin: 12px 0; }
 	.container { padding: 64px 0 0 0; }
 	.filter-container { display: flex; flex-direction: column; height: 100% !important; padding: 12px; }
-	.title { font-weight: 400; }
-		/*position: absolute;
-		bottom: 50px;
-		background-color: rgba(0,0,0,0.5);
-		color: white;
-		font-size: 2em;
-		padding: 20px;
-	}*/
-	.btn { text-transform: initial; }
-	.avatar-container {
-		justify-content: center;
-		align-items: center;
-		position: relative;
-		height: 54px;
-		border-bottom: 3px solid #84FFFF;
-	}
-	.avatar {
-		position: absolute;
-		top: 20%;
-		left: 50%;
-		transform: translate(-50%,0%);
-		border: 3px solid #84FFFF !important;
-	}
-	.card__title--primary {
-		padding-top: 35px !important;
-		text-align: center;
-	}
-	.card__actions { border-top: 1px solid #84FFFF !important; justify-content: center; }
-	.card__actions .btn { border: 1px solid #84FFFF !important; }
+	.filter-title { display: flex; border-bottom: 1px solid #616161; }
+	.filter-title .subheading { padding-left: 6px; font-weight: 500; }
+	.title { font-weight: 400; line-height: initial !important;}
+	.search-box-container { padding-top: 3px !important; }
 	.input-group.primary--text.input-group--focused  { color: #26C6DA !important; }
 	.primary--text input, .primary--text textarea { caret-color: #26C6DA !important; }
+	.input-group { padding: 12px 0 0 !important; }
+	.input-group label { font-size: 14px !important; line-height: 18px !important; }
 </style>
